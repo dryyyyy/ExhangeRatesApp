defd6988ff50f5aeabcff6bd2d3cf78df6c31663
Дима <>
2018-07-14 20:28:18 +0700

    minor fix

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -13,7 +13,7 @@ class ApiController extends Controller
      */
     public function showItem($date){
         $entityManager = $this->getDoctrine()->getManager();
-        $rates = $this->get('app.exRatesService');
+        $rates = $this->get('App\Service\ExRatesService');
         $response = new Response($rates->fetch($entityManager, $date));
         $response->headers->set('Content-Type', 'application/json');
 
